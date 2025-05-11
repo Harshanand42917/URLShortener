5c927df1f322624bb7f306e30571f82495789551
Harshanand42917 <harshanand42917@gmail.com>
2025-05-11 18:42:27 +0530

    Added a url shortener Project

---
+++ Controller/url.js
@@ -0,0 +1,35 @@
+import Url from "../Model/Url.js";
+import shortid from "shortid";
+
+export const shortUrl = async (req, res) => {
+  const longUrl = req.body.longUrl;
+  let shortCode;
+
+  const isExistingUrl = await Url.findOne({ longUrl });
+
+  if (isExistingUrl) {
+    console.log("already existing url");
+    shortCode = isExistingUrl.shortCode;
+  } else {
+    console.log("new url generated");
+    shortCode = shortid.generate();
+    const newUrl = new Url({ shortCode, longUrl });
+    await newUrl.save();
+  }
+
+  const shortUrl = `http://localhost:1000/${shortCode}`;
+  console.log("short saved = ", shortUrl);
+
+  res.render("index.ejs", { shortUrl });
+};
+
+export const getOriginalUrl = async (req, res) => {
+  const shortCode = req.params.shortCode;
+  const originalUrl = await Url.findOne({ shortCode });
+
+  if (originalUrl) {
+    res.redirect(originalUrl.longUrl);
+  } else {
+    res.json({ message: "Invalid shortcode" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import { shortUrl, getOriginalUrl } from "./Controller/url.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// MongoDB Connection
+mongoose
+  .connect(
+    "mongodb+srv://harshanand42917:S0d33GWPg0BTvMBQ@cluster0.i6opsi7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      dbName: "url_shorted_db",
+    }
+  )
+  .then(() => console.log("MongoDb Connected..!"))
+  .catch((err) => console.log(err));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index.ejs", { shortUrl: null });
+});
+
+app.post("/short", shortUrl);
+app.get("/:shortCode", getOriginalUrl);
+
+// Server
+const port = 1000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
